b53f1d0215c752a0fdf82f368bcd6bf905f02bb0
Ayouuuu <arobcher233@163.com>
2022-05-24 00:54:18 +0800

    add: unit test

---
+++ test/test_sdk.py
@@ -0,0 +1,11 @@
+from json.tool import main
+import unittest
+
+
+class TestMethod(unittest.TestCase):
+    def test(self):
+        print("hello world~")
+
+
+if __name__ == "main":
+    unittest.main()
