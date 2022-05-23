307858057785e720912e8e66610c0c3c4c870226
Ayouuuu <arobcher233@163.com>
2022-05-23 16:11:43 +0800

    jkljl

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name = "hniu-jsxsd-sdk",
+    version= "0.1",
+    author= "Ayou",
+    author_email="arobcher233@163.com",
+    packages= find_packages()
+)
