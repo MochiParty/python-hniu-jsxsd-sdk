58cbad7fd1074219a7f8c8afe3e0bfb43935aaa9
Ayouuuu <arobcher233@163.com>
2022-05-28 20:32:04 +0800

    add: 添加打包配置

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+
 setup(
-    name = "hniu-jsxsd-sdk",
-    version= "0.1",
-    author= "Ayou",
+    name="hniu-jsxsd-sdk",
+    version="0.1",
+    author="Ayou",
     author_email="arobcher233@163.com",
-    packages= find_packages()
+    packages=find_packages()
 )
